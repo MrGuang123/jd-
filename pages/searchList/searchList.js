f2998a1da2f6084eaa7e75b13c8940ebdce798b9
MrGuang123 <522125842@qq.com>
2017-01-20 17:57:45 +0800

    添加了搜索列表页面，逻辑不全，待后续处理

--- pages/searchList/searchList.js
+++ pages/searchList/searchList.js
@@ -1,12 +1,49 @@
 
 var app = getApp();
 
+var hotWord = ['手机','女装','男鞋','羽绒服女','男装','羽绒服','女鞋','毛呢大衣女','连衣裙','电视','羽绒服男','女靴'];
+
 Page({
     data:{
-
+        searchContent:undefined,
+        hotWord:hotWord
     },
     onLoad:function(){
-        
+        wx.setNavigationBarTitle({
+            title:'搜索'
+        })
+        wx.showNavigationBarLoading();
+        setTimeout(function(){
+            wx.hideNavigationBarLoading();
+        },1000);
+    },
+    search:function(event){
+        var value = event.detail.value;
+        console.log(event)
+        console.log(value)
+
+        if(event.target.id === '搜索'){
+
+            this.setData({
+                searchContent:undefined
+            })
+            return {
+                value:''    
+            }
+        }else if(event.target.id === '取消'){
+
+            this.setData({
+                searchContent:undefined
+            })
+            return {
+                value:''    
+            }
+        }
+    },
+    keyinput:function(event){
+        this.setData({
+            searchContent:event.detail.value
+        })
     }
 
 })
